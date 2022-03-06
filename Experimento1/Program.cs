581f2b49fa4638c3b64abe3af28c1d6ba5f3f3c9
Carlom2003pt <carlom634@gmail.com>
2022-03-06 19:31:44 +0000

    CriarPastaEscreverFicheiro

---
+++ Experimento1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Experimento1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"C:\teste";
+            string arquivo = "C:\\teste\\teste.txt";
+            //CriarPasta
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+                Console.WriteLine(folderPath);
+            }
+            //Verificar Se Existe
+            if (Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Esta Pasta já existe: " + folderPath);
+            }
+            
+            //Escrever texto no ficheiro
+            if (!File.Exists(arquivo))
+            {
+                using (StreamWriter sw = File.CreateText(arquivo))
+                {
+                    sw.WriteLine("Bem-Vindo!!");
+                    Console.WriteLine("Ficheiro" + arquivo + "criado com sucesso!");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ficheiro Já Existe!");
+                File.Delete(arquivo);
+            }
+        }
+    }
+}
